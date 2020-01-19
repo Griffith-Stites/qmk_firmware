fab1211319279d63bdecd06f7c5c2397ffa6f0fb
Maeve Stites <gjstites@gmail.com>
2020-01-19 01:02:49 -0500

    Create Maeve keyboard layout

---
+++ keyboards/dztech/dz60rgb_wkl/keymaps/maeve/keymap.c
@@ -0,0 +1,36 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // QWERTY main layer
+    [0] = LAYOUT_60_tsangan_hhkb(
+        KC_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,
+        LT(2, KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
+        CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(2),
+        KC_LCTL,        KC_LGUI, KC_LALT,                            KC_SPC,                                      KC_RALT, KC_RGUI, KC_RCTL
+    ),
+    // DVORAK main layer
+    [1] = LAYOUT_60_tsangan_hhkb(
+        KC_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,
+        LT(2, KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
+        CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(2),
+        KC_LCTL,        KC_LGUI, KC_LALT,                            KC_SPC,                                      KC_RALT, KC_RGUI, KC_RCTL
+    ),
+    // Secondary function layer: Function keys, arrows, media control
+    [2] = LAYOUT_60_tsangan_hhkb(
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
+        _______, KC_VOLU, KC_MNXT, KC_MPLY, _______, _______, KC_HOME, KC_PGUP, KC_UP,   KC_PGDN, _______, _______, _______, _______,
+        KC_TRNS, KC_VOLD, KC_MPRV, KC_MUTE, _______, _______, KC_INS, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,          _______,
+        _______,          _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______, _______, KC_TRNS,
+        _______, _______, _______,                            TG(3),                                     _______, _______, _______
+    ),
+    // RGB Control and keyboard configuration - setting main layer and backspace and \ swap, also should have swap
+    [3] = LAYOUT_60_tsangan_hhkb(
+        RESET,   DF(0),   DF(1),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,
+        _______, RGB_TOG, _______, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_MOD, _______, _______, _______, BS_SWAP,
+        _______, _______, _______, _______, _______, _______, _______, _______, RGB_SPI, RGB_SPD, _______, _______,          TG(3),
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______,                            _______,                                     _______, _______, _______
+    )
+};
--- readme.md
+++ readme.md
@@ -13,3 +13,21 @@
 2. Check that keyboard chip shows up with sudo dmesg
 3. Clear memory: sudo dfu-programmer atmega32u4 erase --force
 4. Flash: sudo dfu-programmer atmega32u4 flash kb_default.hex
+
+
+## Layout Notes
+### Pebble (dz60rgb_wkl)
+* Holding caps lock is control
+* Holding tab goes to layer 2 with sectodary functions
+* Can reach the settings layer by clicking space on layer 2
+  * Esc is RESET
+  * 1 is QWERTY
+  * 2 is Dvorak
+  * Backspace swaps backspace and slash
+  * Enter exits
+* Need to add audio, arrow keys, and quantum keycode
+
+
+## Notes
+* BS_SWAP swaps \ and backspace
+* LT(layer, kc) Turn on layer when held, kc when tapped
